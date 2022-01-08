846327b180a6f6b3b5f0f62b1aa4e378eceb643e
Fabio <fabio.delvigna@rstor.io>
2022-01-08 14:44:38 +0100

    Adding some function to manage file content and inodes with immudb

--- pkg/fs/client.go
+++ pkg/fs/client.go
@@ -116,3 +116,52 @@ func (idb *ImmuDbClient) GetChildren(ctx context.Context, parent int64) ([]fuseu
 
 	return dirents, err
 }
+
+// File content can be read as a whole
+func (idb *ImmuDbClient) ReadContent(ctx context.Context, inumber int64) ([]byte, error) {
+	res, err := idb.cl.QueryContext(ctx, "SELECT content from content WHERE inumber=?", inumber)
+	if err != nil {
+		idb.log.Errorf("could not get file %d content: %s", inumber, err)
+
+		return nil, err
+	}
+
+	var content []byte
+
+	defer res.Close()
+	if found := res.Next(); !found {
+		idb.log.Errorf("File %d content not found", inumber)
+
+		return nil, fmt.Errorf("Inode %d not found", inumber)
+	}
+
+	err = res.Scan(content)
+	if err != nil {
+		idb.log.Errorf("could not read file %d content: %s", inumber, err)
+
+		return nil, err
+	}
+
+	return content, err
+}
+
+// File content can be written as a whole
+func (idb *ImmuDbClient) WriteContent(ctx context.Context, inumber int64, data []byte) error {
+	_, err := idb.cl.ExecContext(ctx, "UPSERT INTO content(inumber, content) VALUES(?, ?)", inumber, data)
+	if err != nil {
+		idb.log.Errorf("could not write file %d content: %s", inumber, err)
+	}
+
+	return err
+}
+
+func (idb *ImmuDbClient) WriteInode(ctx context.Context, inode *Inode) error {
+	_, err := idb.cl.ExecContext(ctx, "UPSERT INTO inode(inumber, name, size, nlink, mode, atime, mtime, ctime, crtime, uid, gid, parent) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)",
+		inode.Inumber, inode.Name, inode.Size, inode.Nlink, inode.Mode, inode.Atime, inode.Mtime, inode.Ctime, inode.Crtime, inode.Uid, inode.Gid, inode.Parent)
+	if err != nil {
+		idb.log.Errorf("could not write file %s", inode.Name, err)
+	}
+
+	return err
+}
+
